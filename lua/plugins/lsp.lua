0da65319b4673700cd74b3f174b9d5df349aa40f
Saiful Islam (iMac) <saiful.apm@gmail.com>
2023-07-23 10:06:10 +0600

    eslint fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -103,6 +103,24 @@ return {
           filetypes = { 'html', 'liquid' },
         },
       },
+      setup = {
+        eslint = function()
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            callback = function(event)
+              if not require('lazyvim.plugins.lsp.format').enabled() then
+                -- exit early if autoformat is not enabled
+                return
+              end
+
+              local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = 'eslint' })[1]
+              if client then
+                local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+                if #diag > 0 then vim.cmd('EslintFixAll') end
+              end
+            end,
+          })
+        end,
+      },
     },
   },
   {
